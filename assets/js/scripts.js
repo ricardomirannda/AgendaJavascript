cc67cf66a2a867d811fb881392e2305e99ff2aa4
Ricardo Miranda <ricardomirannda@gmail.com>
2016-06-08 11:12:45 -0300

    ajuste na logica para mostrar os campos que estao pendentes e criacao de uma variavel global para armazenar as mensagens

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,37 +6,37 @@ function adicionarDados(){
 
 	// PEGA O TELEFONE DO USUARIO
 	var telefoneUsuario = document.getElementById('telefoneUsuarioInput').value;
+  var mensagem_erro = "";
 
 
 	// VERIFICAR SE OS CAMPOS NOME E TELEFONE ESTAO VAZIOS
-
-
    for(var i=0; i<contatoForm.elements.length; i++)
 	 {
        if(contatoForm.elements[i].value === "")
 			 {
-				 alert ('Preencha o campo ' + contatoForm.elements[i].name);
-				//  document.querySelector("#contatoForm .messageUser p").style.display="block";;
-	 		 	 contatoForm.elements[i].style.border="solid 1px red";
+				  document.querySelector("#contatoForm .messageUser p").style.display="block";
+					mensagem_erro += contatoForm.elements[i].name + " <br> ";
+					contatoForm.elements[i].style.border="solid 1px red";
        }
 
 			 else
 			 {
 				 contatoForm.elements[i].style.border="none";
+				 document.querySelector("#contatoForm .messageUser p").style.display="none";
 			 }
    }
 
+   document.querySelector("#contatoForm .messageUser p").innerHTML = "Preencha os campos: " +  "<br>" + mensagem_erro;
 
 
-	 // LIMPA OS CAMPOS, APOS OS DADOS SEREM INSERIDOS  e  AJUSTA O FOCO NO NOME
+
+	 // LIMPA OS CAMPOS, APOS OS DADOS SEREM INSERIDOS  e  AJUSTA O FOCO NO NOME e TELEFONE
 		if (nomeUsuario === "")
 		{
 			document.getElementById("nomeUsuarioInput").focus();
 			document.getElementById("nomeUsuarioInput").value = "";
-
 			return false
 		}
-
 
 
 		if (telefoneUsuario === "")
@@ -48,30 +48,29 @@ function adicionarDados(){
 
 
 
-
-	document.contatoForm.reset();
-
-
-	// CRIACAO AUTOMATICA DAS ROWS
-	var table = document.getElementById("tabelaContatos");
-	var row = table.insertRow(1);
-	var colunaCheckBox = row.insertCell(0);
-	var colunaNome = row.insertCell(1);
-	var colunaTelefone = row.insertCell(2);
-	colunaNome.innerHTML = nomeUsuario;
-	colunaTelefone.innerHTML = telefoneUsuario;
+		// LIMPA OS CAMPOS APOS O SUBMIT
+		document.contatoForm.reset();
 
 
+		// CRIACAO AUTOMATICA DAS ROWS
+		var table = document.getElementById("tabelaContatos");
+		var row = table.insertRow(1);
+		var colunaCheckBox = row.insertCell(0);
+		var colunaNome = row.insertCell(1);
+		var colunaTelefone = row.insertCell(2);
+		colunaNome.innerHTML = nomeUsuario;
+		colunaTelefone.innerHTML = telefoneUsuario;
 
-	// CRIACAO AUTOMATICA DO CHECKBOX - SOLUCAO  1
-	var createCheckbox = document.createElement("input");
-	createCheckbox.setAttribute("type", "checkbox");
-	colunaCheckBox.appendChild(createCheckbox);
 
 
-	// CRIACAO AUTOMATICA DO CHECKBOX - SOLUCAO  2
-	// var createCheckbox = document.createElement("input");
-	// createCheckbox.setAttribute("type", "checkbox");
-	// document.querySelector("#tabelaContatos tr > td").appendChild(createCheckbox);
+		// CRIACAO AUTOMATICA DO CHECKBOX - SOLUCAO  1
+		var createCheckbox = document.createElement("input");
+		createCheckbox.setAttribute("type", "checkbox");
+		colunaCheckBox.appendChild(createCheckbox);
+
+		// CRIACAO AUTOMATICA DO CHECKBOX - SOLUCAO  2
+		// var createCheckbox = document.createElement("input");
+		// createCheckbox.setAttribute("type", "checkbox");
+		// document.querySelector("#tabelaContatos tr > td").appendChild(createCheckbox);
 
 }
